42722d7504f6e0a85f2b50c89b47681d6c42fb77
michaelpitop <michaelpitop@outlook.com>
2023-12-06 12:27:34 +0100

    added multiple language support

---
+++ src/lib/i18n.ts
@@ -0,0 +1,37 @@
+import { browser } from '$app/environment';
+import { derived, get } from 'svelte/store';
+import { init, register, locale } from 'svelte-i18n';
+
+const defaultLocale = 'en';
+
+register('en', () => import('./lang/en.json'));
+register('de', () => import('./lang/de.json'));
+
+init({
+	fallbackLocale: defaultLocale,
+	initialLocale: browser ? window.navigator.language : defaultLocale
+});
+
+export function formatDate(date: string | number | Date, options: Intl.DateTimeFormatOptions) {
+	return new Intl.DateTimeFormat(getLocaleForFormat(), options).format(new Date(date));
+}
+
+export function formatNumber(num: number) {
+	return new Intl.NumberFormat(getLocaleForFormat()).format(num);
+}
+
+function getLocaleForFormat() {
+	const selectedLocale = get(locale);
+	switch (selectedLocale) {
+		case 'en':
+			return 'en-US';
+		case 'fr':
+			return 'fr-FR';
+		case 'hi':
+			return 'hi-IN';
+		default:
+			return 'en-US';
+	}
+}
+
+export const isLocaleLoaded = derived(locale, ($locale) => typeof $locale === 'string');
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { isLocaleLoaded } from '$lib/i18n';
+  import { _, locale, locales } from 'svelte-i18n';
+
+  let value: string = 'en';
+
+  function handleLocaleChange(event: any) {
+    event.preventDefault();
+    value = event?.target?.value;
+    $locale = value;
+  }
+</script>
+
+<div class="container">
+  {#if $isLocaleLoaded}
+    <div class="container__title">
+      <h1>{$_('title')}</h1>
+    </div>
+    <div class="container__toggle">
+      <span>{$_('toggle_label')}: </span>
+      <select {value} on:change={handleLocaleChange}>
+        <option value="en" selected>English</option>
+        <option value="de">German</option>
+      </select>
+
+    </div>
+
+  {:else}
+    <div class="container__content">Locale initializing...</div>
+  {/if}
+</div>
