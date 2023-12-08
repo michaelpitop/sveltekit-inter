8c2a5e23b11971b23f37077bfb7110e246e69b4f
michaelpitop <michaelpitop@outlook.com>
2023-12-08 12:54:26 +0100

    trying to add a prehome page for language selection. Still needs work

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
 	import { isLocaleLoaded } from '$lib/i18n';
   import { _, locale, locales } from 'svelte-i18n';
 
-  let value: string = 'en';
+  let value: string = '';
 
   function handleLocaleChange(event: any) {
     event.preventDefault();
     value = event?.target?.value;
     $locale = value;
-    goto('/home');
   }
 </script>
 
 <div class="container">
   {#if $isLocaleLoaded}
-    <!-- <div class="container__title">
+    <div class="container__title">
       <h1>{$_('title')}</h1>
       <h2>{$_('undertitle')}</h2>
-    </div> -->
+    </div>
     <div class="container__toggle">
       <span>{$_('toggle_label')} </span>
       <select {value} on:change={handleLocaleChange}>
