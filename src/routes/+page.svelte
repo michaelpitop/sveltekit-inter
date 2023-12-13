e5e0a37184fbfb7d653fba320a0df2b8391df2c7
michaelpitop <michaelpitop@outlook.com>
2023-12-13 19:22:24 +0100

    added some CSS styles and temporary fix on centering the language buttons...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,12 +13,28 @@
   }
 </script>
 
-<div class="container">
+<div class="buttons-div" >
   {#if $isLocaleLoaded}
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div  class="lang-container">
     
-    <div>
       <button on:click={handleLocaleChange} value="en">English</button>
       <button on:click={handleLocaleChange} value="de">Deutsch</button>
+      
     </div>
 
   {:else}
@@ -26,3 +42,37 @@
   {/if}
 </div>
 
+<style>
+  .buttons-div {
+    width: 100%;
+    height: 100%;
+  }
+
+  .lang-container  {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+
+  }
+  button {
+    border: none;
+    color: black;
+    padding: 16px 32px;
+    text-align: center;
+    font-size: 100px;
+    margin: 4px 2px;
+    transition: 0.3s;
+    opacity: 0.7;
+    display: inline-block;
+    text-decoration: none;
+    cursor: pointer;   
+    border-radius: 15%; 
+  }
+  button:hover {
+    outline-color: transparent;
+    outline-style:solid;
+    box-shadow: 0 0 0 4px #5a01a7;
+    transition: 0.7s;
+  }
+</style>
